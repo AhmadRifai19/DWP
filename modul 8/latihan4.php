186a977ec3ebdb343ef71d4bda43406ef47d9c86
AhmadRifai19 <ahmadrifai34256@gmail.com>
2026-04-20 09:27:57 +0700

    menyelesaikan latihan 6 modul 8

---
+++ modul 8/latihan4.php
@@ -0,0 +1,39 @@
+<?php
+/* Operator logika yang bisa digunakan
+ * ==    Sama Dengan        $x == $y
+ * ===   Identical          $x === $y
+ * !=    Tidak sama dengan  $x != $y
+ * <>    Tidak sama dengan  $x <> $y
+ * !==   Not identical      $x !== $y
+ * >     Lebih Besar dari   $x > $y
+ * <     Kurang Dari        $x < $y
+ * >=    Lebih besar atau Sama dengan  $x >= $y
+ * <=    Kurang dari atau sama dengan  $x <= $y
+ * <=>   Spaceship          $x <=> $y
+ */
+
+$t = date("H"); // mendapatkan jam dengan format 1-24
+echo "If<br>";
+if ($t < 16) {
+    echo "Selamat siang!";
+}
+
+// Tambahkan kode dibawah ini if else
+$t = date("H"); // mendapatkan jam dengan format 1-24
+echo "<br> If dan Else <br>";
+if ($t < 20) {
+    echo "Selamat siang!";
+} else {
+    echo "Selamat malam!";
+}
+
+// Tambahkan kode dibawah ini
+echo "<br> Nested If <br>";
+if ($t < 12) {
+    echo "Selamat Pagi!";
+} elseif ($t < 16) {
+    echo "Selamat sore!";
+} else {
+    echo "Selamat Malam!";
+}
+?>
