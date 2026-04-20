c082148889b98c001aa9a623ab7d01a06a9dfaec
AhmadRifai19 <ahmadrifai34256@gmail.com>
2026-04-20 08:42:26 +0700

    menyelesaikan latihan 4 modul 8

---
+++ modul 8/latihanVariabel.php
@@ -0,0 +1,13 @@
+<?php
+$Senin = "Senin";
+$Selasa = "Selasa";
+$Rabu = "Rabu";
+$Kamis = "Kamis";
+$Jumat = "Jumat";
+$Sabtu = "Sabtu";
+$Minggu = "Minggu";
+
+echo "Dalam satu minggu terdapat 7 hari, yaitu ", $Senin," ", $Selasa," ", $Rabu," ", $Kamis," ", $Jumat," ", $Sabtu," ", $Minggu, ".";
+
+
+?>
