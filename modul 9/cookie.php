28fe2502146ffd9a98e05f6371052ed96a929ce6
AhmadRifai19 <ahmadrifai34256@gmail.com>
2026-05-11 09:42:07 +0700

    Menyelesaikan Modul 9 Latihan 6

---
+++ modul 9/cookie.php
@@ -0,0 +1,22 @@
+<?php
+$cookie_name = "user";
+$cookie_value = "253307049";
+setcookie($cookie_name, $cookie_value, time() + (86400 * 30), "/");
+?>
+<!DOCTYPE html>
+<html lang="id">
+<head>
+    <meta charset="UTF-8">
+    <title>Latihan Cookies</title>
+</head>
+<body>
+<?php
+if(!isset($_COOKIE[$cookie_name])) {
+    echo "Cookie named '" . $cookie_name . "' is not set!";
+} else {
+    echo "Cookie '" . $cookie_name . "' is set!<br>";
+    echo "Value is: " . $_COOKIE[$cookie_name];
+}
+?>
+</body>
+</html>
